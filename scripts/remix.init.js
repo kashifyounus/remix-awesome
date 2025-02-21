499afad2bdddfc7636f1d8e5034a91c1cb3080db
kashifyounus <mkashifyounus@hotmail.com>
2025-02-21 15:48:54 +0500

    updated init script

--- scripts/remix.init.js
+++ scripts/remix.init.js
@@ -1,12 +1,22 @@
 import { execSync } from "child_process";
 import fs from "fs";
+import path from "path";
+import process from "process";
+
+// Function to delete the scripts folder
+const deleteScriptsFolder = () => {
+  const scriptsPath = path.join(process.cwd(), "scripts");
+  if (fs.existsSync(scriptsPath)) {
+    fs.rmSync(scriptsPath, { recursive: true, force: true });
+  }
+};
 
 console.log("🚀 Setting up your Remix project...");
 
 // Install dependencies
 execSync("npm install", { stdio: "inherit" });
 
-// Remove the `scripts/` folder (to clean up)
-fs.rmSync("scripts", { recursive: true, force: true });
+// Remove the scripts folder
+deleteScriptsFolder();
 
-console.log("✅ Setup complete! Run 'npm run dev' to start your remix awesome.");
+console.log("✅ Setup complete! Run 'npm run dev' to start your Remix app.");
